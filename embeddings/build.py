44303078b0164e57ce387af00b40bcd694e1c14e
John Lam <jflam@microsoft.com>
2023-02-03 20:47:42 -0800

    feat: adding PDF support to index builder

--- embeddings/build.py
+++ embeddings/build.py
@@ -3,6 +3,7 @@ import time
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain import FAISS
+from PyPDF2 import PdfReader
 
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size = 500,
@@ -11,16 +12,22 @@ text_splitter = RecursiveCharacterTextSplitter(
 )
 
 EMBEDDING_MODEL = "sentence-transformers/gtr-t5-large"
-INPUT_FILE = "avg.txt"
+INPUT_FILE = "react.pdf"
 INDEX_FILE = "index.pkl"
 MAPPING_FILE = "mappings.pkl"
 DOCUMENTS_FILE = "documents.pkl"
 
-print(f"Loading {INPUT_FILE}...")
-with open(INPUT_FILE) as f:
-    lines = f.readlines()
+def read_file(filename: str) -> str:
+    print(f"Loading {filename}...")
+    if filename.endswith(".pdf"):
+        with open(filename, "rb") as f:
+            pdf = PdfReader(f)
+            return "".join([pdf.pages[i].extract_text() for i in range(len(pdf.pages))])
+    else:
+        with open(filename, "r") as f:
+            return f.read()
 
-doc = "".join(lines)
+doc = read_file(INPUT_FILE)
 texts = text_splitter.split_text(doc)
 
 print(f"Loading embedding model {EMBEDDING_MODEL}...")
